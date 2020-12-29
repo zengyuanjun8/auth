a0a4bf8fa15b3de730774f33726929f1a9df99eb
zyj <972263378@qq.com>
2020-12-30 06:51:46 +0800

    接入nacos注册中心，整合spring cloud

---
+++ src/main/java/com/zyj/nacos/controller/EchoController.java
@@ -0,0 +1,24 @@
+package com.zyj.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class EchoController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public EchoController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    public String echo(@PathVariable String str) {
+        return restTemplate.getForObject("http://answer/ip/my", String.class) + " - " + str;
+    }
+}
